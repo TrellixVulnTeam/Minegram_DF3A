f9f87879b5cdb8ceb3044b44974b62b01ed764db
isaellara <isael_contato@hotmail.com>
2021-03-16 11:57:43 -0300

    Update extract_users.py

--- extract_users.py
+++ extract_users.py
@@ -1,11 +1,12 @@
 import instaloader
+from instaloader import Instaloader
 
 
 class ExtractUsers:
-    def __init__(self, profile_to_extract):
-        self._profile_to_extract = instaloader.Profile.\
-            from_username(instaloader.Instaloader().context, profile_to_extract)
-        self._list_of_extracted_users = []
+    def __init__(self, accont_logged, profile_to_extract):
+        self.login = Instaloader.login(accont_logged._user, accont_logged._password)
+        self._profile = instaloader.Profile.from_username(self.login.context, profile_to_extract)
+        self._list_of_extracted = []
 
     def config_extract(self, number_of_users_per_comment, hard_extract=False):
         if number_of_users_per_comment == 1:
@@ -18,7 +19,7 @@ class ExtractUsers:
 
     def check_profile(self):
         try:
-            if self._profile_to_extract.followers > 100:
+            if self._profile.followers > 100:
                 return True
             else:
                 return False
@@ -37,8 +38,8 @@ class ExtractUsers:
 
     def remove_user0(self):
         try:
-            if self.is_invalid_user(self._list_of_extracted_users[0]) is True:
-                self._list_of_extracted_users.remove(self._list_of_extracted_users[0])
+            if self.is_invalid_user(self._list_of_extracted[0]) is True:
+                self._list_of_extracted.remove(self._list_of_extracted[0])
                 return True
             else:
                 return False
@@ -47,8 +48,8 @@ class ExtractUsers:
 
     def remove_user1(self):
         try:
-            if self.is_invalid_user(self._list_of_extracted_users[1]) is True:
-                self._list_of_extracted_users.remove(self._list_of_extracted_users[1])
+            if self.is_invalid_user(self._list_of_extracted[1]) is True:
+                self._list_of_extracted.remove(self._list_of_extracted[1])
                 return True
             else:
                 return False
@@ -57,8 +58,8 @@ class ExtractUsers:
 
     def remove_user2(self):
         try:
-            if self.is_invalid_user(self._list_of_extracted_users[2]) is True:
-                self._list_of_extracted_users.remove(self._list_of_extracted_users[2])
+            if self.is_invalid_user(self._list_of_extracted[2]) is True:
+                self._list_of_extracted.remove(self._list_of_extracted[2])
                 return True
             else:
                 return False
@@ -70,8 +71,8 @@ class ExtractUsers:
     def get_list_of_followers(self):
         try:
             if self.check_profile() is True:
-                for user in self._profile_to_extract.get_followers():
-                    self._list_of_extracted_users.append('@'+user.username)
+                for user in self._profile.get_followers():
+                    self._list_of_extracted.append('@'+user.username)
                 return True
             else:
                 return False
@@ -85,8 +86,8 @@ class ExtractUsers:
             if hard_extract is True and self.remove_user0() is True:
                 pass
             else:
-                follower.append(self._list_of_extracted_users[0])
-                self._list_of_extracted_users.remove(self._list_of_extracted_users[0])
+                follower.append(self._list_of_extracted[0])
+                self._list_of_extracted.remove(self._list_of_extracted[0])
                 return follower[0]
         except():
             return False
@@ -98,9 +99,9 @@ class ExtractUsers:
             if hard_extract is True and self.remove_user0() is True or self.remove_user1() is True:
                 pass
             else:
-                followers.append(self._list_of_extracted_users[0] + ' ' + self._list_of_extracted_users[1])
-                self._list_of_extracted_users.remove(self._list_of_extracted_users[0])
-                self._list_of_extracted_users.remove(self._list_of_extracted_users[1])
+                followers.append(self._list_of_extracted[0] + ' ' + self._list_of_extracted[1])
+                self._list_of_extracted.remove(self._list_of_extracted[0])
+                self._list_of_extracted.remove(self._list_of_extracted[1])
                 return followers[0]
         except():
             return False
@@ -113,11 +114,11 @@ class ExtractUsers:
                     or self.remove_user1() is True or self.remove_user2() is True:
                 pass
             else:
-                followers.append(self._list_of_extracted_users[0] + ' ' + self._list_of_extracted_users[1]
-                                 + ' ' + self._list_of_extracted_users[2])
-                self._list_of_extracted_users.remove(self._list_of_extracted_users[0])
-                self._list_of_extracted_users.remove(self._list_of_extracted_users[1])
-                self._list_of_extracted_users.remove(self._list_of_extracted_users[2])
+                followers.append(self._list_of_extracted[0] + ' ' + self._list_of_extracted[1]
+                                 + ' ' + self._list_of_extracted[2])
+                self._list_of_extracted.remove(self._list_of_extracted[0])
+                self._list_of_extracted.remove(self._list_of_extracted[1])
+                self._list_of_extracted.remove(self._list_of_extracted[2])
                 return followers[0]
         except():
             return False
